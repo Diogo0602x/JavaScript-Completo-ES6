1c2811acd375c6c5e6e9941ce894cbfe44765684
Diogo0602x <diogoteixeira0602@gmail.com>
2021-06-21 17:14:05 -0300

    Aula 0201 terminada

---
+++ script.js
@@ -0,0 +1,21 @@
+// Declarar uma variável com o seu nome
+var nome = 'André';
+
+// Declarar uma variável com a sua idade
+var idade = 28;
+
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+var comidaFavorita;
+
+// Atribuir valor a sua comida favorita
+comidaFavorita = 'Pizza';
+
+// Declarar 5 variáveis diferentes sem valores
+var time = 'Vasco',
+  professor,
+  pais,
+  endereco,
+  rua;
+
+console.log(nome, idade, comidaFavorita);
