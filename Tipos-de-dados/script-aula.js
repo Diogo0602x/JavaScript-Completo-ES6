ccda37639daaa2ba21bc100efafe63227b3e54e0
Diogo0602x <diogoteixeira0602@gmail.com>
2021-06-23 10:25:52 -0300

    Tipos de dados feitos

---
+++ Tipos-de-dados/script-aula.js
@@ -0,0 +1,28 @@
+var idade = 28;
+var simbolo = Symbol();
+console.log(typeof simbolo);
+
+var nome = 'Andre';
+var sobrenome = 'Rafael';
+var nomeCompleto = nome + ' ' + sobrenome;
+
+console.log(nomeCompleto);
+
+// var gols = 1000;
+// var frase = 'Romário fez ' + gols + ' gols';
+// console.log(typeof frase);
+
+var ano = '2018';
+var mes = 8;
+console.log(ano + mes);
+
+var melhor = 'teste';
+var frase1 = "Esse é o \"melhor\" jogo";
+console.log(frase1);
+
+var gols = 1000;
+var frase2 = `Romário fez ${gols * 2} gols`;
+
+console.log(frase2);
+
+
---
+++ Tipos-de-dados/script.js
@@ -0,0 +1,23 @@
+// Declare uma variável contendo uma string
+var nome = 'André';
+
+// Declare uma variável contendo um número dentro de uma string
+var ano = '2018';
+
+// Declare uma variável com a sua idade
+var idade = 28;
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+var sobrenome = 'Rafael'
+var nomeCompleto = `${nome} ${sobrenome}`;
+
+// Coloque a seguinte frase em uma variável: It's time
+var frase = `It's time`;
+
+// Verifique o tipo da variável que contém o seu nome
+var verificarTipoNome = typeof nome;
+
+console.log(verificarTipoNome);
+
+// apagar
