533f9535218cc0910f7ae872bd37884a10d8e859
Diogo0602x <diogoteixeira0602@gmail.com>
2021-06-23 10:26:24 -0300

    Numero e operadores feitos

---
+++ Numero-e-operadores/script-aula.js
@@ -0,0 +1,20 @@
+var total1 = 10 + 5 + 10;
+var divisao = 100 / 5;
+var modulo = 3872983892 % 3;
+
+console.log(modulo);
+
+var testeNaN = '100' / 2;
+console.log(isNaN(testeNaN));
+
+var soma1 = (10 + 10) + 20 + 30 * 4 / (2 + 10);
+console.log(soma1);
+
+var x = 5;
+console.log(x--);
+console.log(x);
+
+var idade = +'28';
+var somaIdade = -5;
+
+console.log(idade);
---
+++ Numero-e-operadores/script.js
@@ -0,0 +1,22 @@
+// Qual o resultado da seguinte expressão?
+var total = 10 + 5 * 2 / 2 + 20;
+
+// Crie duas expressões que retornem NaN
+var expressao1 = 'Teste' / 2;
+var expressao2 = 'Teste' - 2;
+console.log(expressao1, expressao2);
+
+// Somar a string '200' com o número 50 e retornar 250
+var soma = +'200' + 50;
+console.log(soma);
+
+// Incremente o número 5 e retorne o seu valor incrementado
+var x = 5;
+console.log(++x);
+
+// Como dividir o peso por 2?
+var numero = +'80' / 2;
+var unidade = 'kg';
+var peso = numero + unidade; // '80kg'
+
+console.log(peso);
