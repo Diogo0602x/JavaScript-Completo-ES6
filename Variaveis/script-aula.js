8c89a9a101463ced908154bdcfb91437be652092
Diogo0602x <diogoteixeira0602@gmail.com>
2021-06-23 10:25:16 -0300

    Variaveis feitas

---
+++ Variaveis/script-aula.js
@@ -0,0 +1,31 @@
+var nome = 'André';
+var idade = 28;
+var possuiFaculdade = true;
+
+console.log(nome, idade, possuiFaculdade, nome);
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = totalComprado * preco;
+
+console.log(totalPreco);
+
+var sobrenome = 'Rafael',
+    cidade = 'Rio';
+
+console.log(sobrenome, cidade);
+
+var semDefinir;
+console.log(semDefinir);
+
+// console.log(aindaNaoDefine);
+var comida;
+console.log(comida);
+comida = 'Pizza';
+console.log(comida);
+
+// const time = 'Vasco';
+// time = 'Flamengo';
+
+// console.log(time);
+
