c69cdee719472afd97bc714cf369e6314e288fbf
Diogo Sales Teixeira <diogoteixeira0602@gmail.com>
2022-02-25 09:31:10 -0300

    Seleção de Elementos 1

---
+++ script.js
@@ -0,0 +1,15 @@
+const animais = document.getElementById('animais');
+console.log(animais);
+
+const gridSection = document.getElementsByClassName('grid-section');
+
+console.log(gridSection[1]);
+
+const primeiraUl = document.querySelector('ul');
+console.log(primeiraUl);
+
+const linkInterno = document.querySelector('[href^="#"]');
+console.log(linkInterno)
+
+const animaisImg = document.querySelectorAll('.animais img');
+console.log(animaisImg[0]);
