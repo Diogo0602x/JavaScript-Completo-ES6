f7bb8790fca498806051a37df8e3689a9c3fc70d
Diogo Sales Teixeira <diogoteixeira0602@gmail.com>
2022-03-02 16:05:33 -0300

    Dimensões e Distâncias 2

--- script.js
+++ script.js
@@ -1,35 +1,45 @@
-const listaAnimais = document.querySelector('.animais-lista');
+// Verifique a distância da primeira imagem
+// em relação ao topo da página
+const img = document.querySelector('img');
+const imgTop = img.offsetTop;
 
-const height = listaAnimais.scrollHeight;
-const animaisTop = listaAnimais.offsetTop;
-console.log(animaisTop);
+console.log(imgTop);
 
-const primeiroh2 = document.querySelector('h2');
-const h2left = primeiroh2.offsetLeft;
-
-const h2rect = primeiroh2.getBoundingClientRect();
-
-console.log(h2rect.top);
-
-if(h2rect.top < 0 ) {
-  console.log('Passou do elemnto');
+// Retorne a soma da largura de todas as imagens
+function somaImagens() {
+  const imagens= document.querySelectorAll('img');
+  let soma = 0;
+  imagens.forEach((imagem) => {
+    soma += imagem.offsetWidth;
+  });  
+  console.log(soma);
 }
 
-console.log(
-  window.innerWidth,
-  window.innerHeight,
-  window.outerWidth,
-  window.outerHeight,
-
-  window.pageYOffset,
-  window.pageXOffset,
-);
-
-const small = window.matchMedia('(max-width: 600px)').matches;
-
-if(small) {
-  console.log('Usuário mobile');
-} else {
-  console.log('Usuário desktop');
+window.onload = function() {
+  somaImagens();
 }
 
+// Verifique se os links da página possuem
+// o mínimo recomendado para telas utilizadas
+// com o dedo. (48px/48px de acordo com o google)
+const links = document.querySelectorAll('a');
+
+links.forEach((link) => {
+  const linkWidth = link.offsetWidth;
+  const linkHeight = link.offsetHeight;
+  if(linkWidth >= 48 && linkHeight >= 48) {
+    console.log(link, 'Possui boa acessibilidade')
+  } else {
+    console.log(link, 'Não possui boa acessibilidade')
+  }
+});
+
+// Se o browser for menor que 720px,
+// adicione a classe menu-mobile ao meni
+
+const browserSmall = window.matchMedia('(max-width: 720px').matches;
+
+if(browserSmall) {
+  const menu = document.querySelector('.menu');
+  menu.classList.add('menu-mobile');
+}
