889b36fd2eb56b46fa75676adbc6694b6349077a
Diogo Sales Teixeira <diogoteixeira0602@gmail.com>
2022-03-03 12:09:21 -0300

    Traversing e Manipulação 1

--- script.js
+++ script.js
@@ -1,45 +1,15 @@
-// Quando o usuário clicar nos links internos do site,
-// adicione a classe ativo ao item clicado e remova dos
-// demais itens caso eles possuam a mesma. Previna
-// o comportamento padrão desses links
+const h1 = document.querySelector('h1');
+const animaisLista = document.querySelector('.animais-lista');
 
-const linksInternos = document.querySelectorAll('a[href^="#"]');
+console.log(h1.innerHTML);
+console.log(h1.outerHTML);
 
-function handleLink(event) {
-  event.preventDefault();
-  linksInternos.forEach((link) => {
-    link.classList.remove('ativo');
-  });
-  event.currentTarget.classList.add('ativo');
-}
+// h1.outerHTML = '<p>Novo Título </p>';
+//console.log(animaisLista.innerHTML);
 
-linksInternos.forEach((link) => {
-  link.addEventListener('click', handleLink);
-});
+const lista = document.querySelector('.animais-lista');
 
+console.log(lista.children[--lista.children.lenght]);
+console.log(lista.querySelector('li:last-child'));
 
-// Selecione todos os elementos do site começando a partir do body,
-// ao clique mostre exatamente quais elementos estão sendo clicados
-const todosElementos = document.querySelectorAll('body *');
-
-function handleElemento(event) {
-  event.currentTarget.remove();
-}
-
-// todosElementos.forEach((elemento) => {
-//  elemento.addEventListener('click', handleElemento);
-// })
-
-
-// Utilizando o código anterior, ao invés de mostrar no console,
-// remova o elemento que está sendo clicado, o método remove () remove um elemento
-
-// Se o usuário clicar na tecla (t), aumente todo o texto do site.
-function handleClickT(event) {
-  console.log(event.key);
-  if(event.key === 't') {
-    document.documentElement.classList.toggle('textoMaior');
-  }
-}
-
-window.addEventListener('keydown', handleClickT);
+console.log(lista.previousSibling);
